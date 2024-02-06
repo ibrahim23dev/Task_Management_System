d8c2fe2ad31efd31c06fd99c0e34239f14178c62
Ibrahim <ibrahim.soft.dev23@gmail.com>
2024-02-06 16:14:47 +0600

    Inshallah I will try more and more

---
+++ client/src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function app() {
+  return (
+    <div>
+     <h1 className="text-3xl font-bold underline">
+     
+    </h1>
+    </div>
+  )
+}
+
+export default app
---
+++ client/src/views/Auth/Registation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Registation() {
+  return (
+    <div>
+      Registation
+    </div>
+  )
+}
+
+export default Registation
